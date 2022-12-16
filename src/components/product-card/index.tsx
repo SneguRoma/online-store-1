2740d435d621e73072b92d41d18c4e062e264d66
Faz-R <FazaR92@gmail.com>
2022-12-16 21:26:00 +0300

    feat: add product card and text

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -1,10 +1,19 @@
-import { products } from "../../data"
+import { IProduct } from "../../interfaсes"
+import './index.css';
 
-const Product: React.FC = (props: any) => {
-  return <form className='flex justify-center'>
-    <h1 className=''>Title</h1>
-    <div>Indo</div>
-  </form>
+interface ProductProps {
+  product: IProduct;
 }
 
-export {Product}
+export function Product (props: ProductProps) {
+  return <form className=''>
+    <h2 className=''>{ props.product.title }</h2>
+    <div>{ props.product.brand}</div>
+    <div>{ props.product.price }</div>
+    <div>{ props.product.rating }</div>
+    <div>{ props.product.stock }</div>
+    <div>{ props.product.discountPercentage }</div>
+    <div>{ props.product.category}</div>
+    <div>{ props.product.description}</div>
+  </form>
+}
