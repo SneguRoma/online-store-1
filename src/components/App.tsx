c6d53e1628446f5d804e05199362558753f8de75
Faz-R <FazaR92@gmail.com>
2023-01-04 00:39:19 +0300

    feat: add change from mainPage brunch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,7 @@
-import React from 'react';
-import { Found } from "./Found"
-
 const App: React.FC = () => {
-<<<<<<< HEAD
   return (
     <div></div>
   )
-=======
-  return <div>
-    <Found/>
-
-  </div>
->>>>>>> ade05369aac64fa0db1b4571f3c29d5e60278aa2
 }
 
 export {App}
