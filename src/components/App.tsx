82c97d6669ef9a1e4109274df35c0b0a5342a0f4
Faz-R <FazaR92@gmail.com>
2023-01-05 23:58:22 +0300

    fix: actual version package.json

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from "react"
+import { BrowserRouter, Route } from "react-router-dom"
 import { Found } from "../pages/Found"
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Found/>
-    </div>
+    <BrowserRouter>
+    <Route>
+      
+    </Route>
+    </BrowserRouter>
   )
 }
 
