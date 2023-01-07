c1d4e4172d8de5734b20588714768557af50bb39
Faz-R <FazaR92@gmail.com>
2023-01-07 16:22:57 +0300

    feat: add persist for cart items

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import './normalize.css';
 import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
-import { store } from './redux';
+import { store, persistor } from './redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,9 +13,13 @@ const root = ReactDOM.createRoot(
 
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 
